68df56a0f84389d118a9f3d3578e17845e9155ba
sanyamsah <sanyamsah25@gmail.com>
2024-07-23 18:08:36 +0530

    middleware and util

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        // uploading the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        });
+        // file has been uploaded successfully
+        console.log("File uploaded on cloudinary.");
+        console.log(response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // removing the locally saved file when upload fails
+        return null;
+    }
+}
+
+export { uploadOnCloudinary };
